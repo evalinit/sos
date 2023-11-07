e031d2c644876cd3205ce78b030ef6f987d47caa
JT <skatty14us@gmail.com>
2023-11-07 16:53:44 -0700

    Create test2.js

---
+++ src/test2.js
@@ -0,0 +1,19 @@
+// Sample JavaScript file
+
+// Variables
+let number1 = 10;
+let number2 = 5;
+
+// Function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// Function to subtract two numbers
+function subtractNumbers(a, b) {
+    return a - b;
+}
+
+// Function calls
+console.log(`The sum of ${number1} and ${number2} is: ${addNumbers(number1, number2)}`);
+console.log(`The difference between ${number1} and ${number2} is: ${subtractNumbers(number1, number2)}`);
