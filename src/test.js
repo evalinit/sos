7babe14a5e7f69e97434668fea0e37a5744705ca
JT <skatty14us@gmail.com>
2023-11-07 15:54:06 -0700

    Create test.js

---
+++ src/test.js
@@ -0,0 +1,13 @@
+// Sample JavaScript file
+
+// Declaring variables
+let name = "John";
+let age = 30;
+
+// Function to greet a person
+function greetPerson(name, age) {
+    console.log(`Hello, ${name}! You are ${age} years old.`);
+}
+
+// Calling the function to greet the person
+greetPerson(name, age);
